f6f561a133fc2a987f6a0aaebc5a15003c76300a
akashsingla786 <singlaa264@gmail.com>
2024-01-30 22:09:44 +0530

    endpoint creation.

--- core/src/main/java/org/vriksha/core/controller/SignUpController.java
+++ core/src/main/java/org/vriksha/core/controller/SignUpController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 
 @Controller
+@RequestMapping("/user")
 public class SignUpController {
 
     @PostMapping("/signUp")
